94b3a3d7103702c78e797477754d011ecf232047
Ziad Salah <zslah1935@gmail.com>
2024-06-09 14:10:49 +0300

    family members completed

---
+++ lib/models/family_model.dart
@@ -0,0 +1,75 @@
+class FamilyModel {
+  final String jpName;
+  final String enName;
+  final String imgPath;
+  final String soundPath;
+  FamilyModel({
+    required this.jpName,
+    required this.enName,
+    required this.imgPath,
+    required this.soundPath,
+  });
+}
+
+List<FamilyModel> family = [
+  FamilyModel(
+    jpName: 'chich',
+    enName: 'father',
+    imgPath: 'assets/images/family_members/family_father.png',
+    soundPath: 'sounds/family_members/father.wav',
+  ),
+  FamilyModel(
+    jpName: 'chich',
+    enName: 'Mother',
+    imgPath: 'assets/images/family_members/family_mother.png',
+    soundPath: 'sounds/family_members/mother.wav',
+  ),
+  FamilyModel(
+    jpName: 'chich',
+    enName: 'Daughter',
+    imgPath: 'assets/images/family_members/family_daughter.png',
+    soundPath: 'sounds/family_members/daughter.wav',
+  ),
+  FamilyModel(
+    jpName: 'chich',
+    enName: 'Grand father',
+    imgPath: 'assets/images/family_members/family_grandfather.png',
+    soundPath: 'sounds/family_members/grand father.wav',
+  ),
+  FamilyModel(
+    jpName: 'chich',
+    enName: 'Grand mother',
+    imgPath: 'assets/images/family_members/family_grandmother.png',
+    soundPath: 'sounds/family_members/grand mother.wav',
+  ),
+  FamilyModel(
+    jpName: 'chich',
+    enName: 'Older brother',
+    imgPath: 'assets/images/family_members/family_older_brother.png',
+    soundPath: 'sounds/family_members/older brother.wav',
+  ),
+  FamilyModel(
+    jpName: 'chich',
+    enName: 'Older sister',
+    imgPath: 'assets/images/family_members/family_older_sister.png',
+    soundPath: 'sounds/family_members/older sister.wav',
+  ),
+  FamilyModel(
+    jpName: 'chich',
+    enName: 'Younger brother',
+    imgPath: 'assets/images/family_members/family_younger_brother.png',
+    soundPath: 'sounds/family_members/younger brother.wav',
+  ),
+  FamilyModel(
+    jpName: 'chich',
+    enName: 'Younger sister',
+    imgPath: 'assets/images/family_members/family_younger_sister.png',
+    soundPath: 'sounds/family_members/younger sister.wav',
+  ),
+  FamilyModel(
+    jpName: 'chich',
+    enName: 'Son',
+    imgPath: 'assets/images/family_members/family_son.png',
+    soundPath: 'sounds/family_members/son.wav',
+  ),
+];
--- lib/screens/family_members_page.dart
+++ lib/screens/family_members_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:language_learning_app/features/custom_appbar.dart';
+import 'package:language_learning_app/features/family_member_item.dart';
+import 'package:language_learning_app/models/family_model.dart';
 
 class FamilyMembersPage extends StatelessWidget {
   const FamilyMembersPage({super.key});
@@ -8,6 +10,14 @@ class FamilyMembersPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar('Family Members'),
+      body: ListView.builder(
+        itemCount: family.length,
+        itemBuilder: (context, index) {
+          return FamilyMemberItem(
+            model: family[index],
+          );
+        },
+      ),
     );
   }
 }
